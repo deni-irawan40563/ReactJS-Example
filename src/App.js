7cdaf27cd735522764676b4992b7db4a7c17afd7
Deni Irawan Nugraha <deni.irawan40563@gmail.com>
2021-03-01 09:21:35 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+// import {Link} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="ButtonLink">StateFul Component</h1>
+      <h1 className="ButtonLink">StateLess Component</h1>
+      <h1 className="ButtonLink">Props</h1>
+      <h1 className="ButtonLink">LifeCycle</h1>
+      <h1 className="ButtonLink">Connect BackEnd</h1>
+      <h1 className="ButtonLink">React Router</h1>
+      <h1 className="ButtonLink">LifeCycle</h1>
+      <h1 className="ButtonLink">Connect BackEnd</h1>
+      <h1 className="ButtonLink">React Router</h1>
+    </div>
+  );
+}
+
+export default App;
